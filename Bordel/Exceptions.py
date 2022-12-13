15e9e3d20af99f1828cd1c00f244f26ef3b00eb6
JuiceKo <94040097+JuiceKo@users.noreply.github.com>
2022-12-13 07:19:47 +0100

    Update Exceptions.py

--- Bordel/Exceptions.py
+++ Bordel/Exceptions.py
@@ -8,4 +8,27 @@ else:
     print(ageUtilisateur)
 finally:
     print("Peut import ce qu'il se passe, j'affiche ce message car c'est un finally")
+    
+    
+    if __name__ == "main":
+    try:
+        with open ("textfile.txt", "r") as f:
+            for i in f:
+                l= l.rstrip("\n\r")
+                print(l)
+    except FileNotFoundError:
+        print("File not found")
+    except PermissionError:
+        print("You don't have permission to read this file")
+    except FileExistsError:
+        print("File already exists")
+    except IOError:
+        print("File not found") 
+    else:
+        print("main")
+    finally:
+        with open ("textefile.txt", "r") as f:
+            for i in f:
+                l= l.rstrip("\n\r")
+                print(l)
 
